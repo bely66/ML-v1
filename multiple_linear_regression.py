97347101e49676e20da014beee6eaa915e722fb5
mohamed nabil <medo661998@gmail.com>
2019-04-07 21:09:21 +0200

    multiple linear regression
    using multiple linear regression techniques on a buissness problem

---
+++ multiple_linear_regression.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr  5 17:37:34 2019
+
+@author: mohamed_nabil
+"""
+# always check for assumptions of linear regression
+# 1- linearity / 2- homoscedasticity
