ac568422757d0a7b654e88ddd8c6132adc31551c
mohamed nabil <medo661998@gmail.com>
2019-04-08 14:40:31 +0200

    changed directory

--- multiple_linear_regression.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Apr  5 17:37:34 2019
-
-@author: mohamed_nabil
-"""
-# always check for assumptions of linear regression
-# 1- linearity / 2- homoscedasticity
