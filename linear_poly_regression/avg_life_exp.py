a64cddebfbe2b8461863b6c9c9aa35891dcac776
mohamed nabil <medo661998@gmail.com>
2019-04-08 14:34:21 +0200

    calculating avg life expect
    using linear regression

---
+++ linear_poly_regression/avg_life_exp.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr  8 13:25:05 2019
+
+@author: mohamed nabil
+"""
+
+import numpy as np 
+import pandas as pd
+import matplotlib.pyplot as plt
+
+data = pd.read_csv('bmi_data.csv')
+
+X = data.iloc[:,1:-1]
+Y= data.iloc[:,-1]
+
+from sklearn.linear_model import LinearRegression
+regress = LinearRegression()
+regress.fit(X,Y)
+
+y_pred = regress.predict(X)
+
+plt.scatter(X,Y,color='red')
+plt.plot(X,y_pred,color='red')
+plt.show()
+
