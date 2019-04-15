24e598dbf586c4ff7038998e37489bd0b53f8470
mohamed nabil <medo661998@gmail.com>
2019-04-15 04:14:35 +0200

    first trial of svr

---
+++ SVR/svr_algorithm.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 15 02:50:11 2019
+
+@author: mohamed nabil
+"""
+import numpy as np 
+import pandas as pd 
+import matplotlib.pyplot as plt 
+file = pd.read_csv("Position_Salaries.csv")
+
+x = file.iloc[:,1:2].values
+y= file.iloc[:,2:].values
+
+#applying feature scaling 
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+x = sc_X.fit_transform(x)
+sc_Y = StandardScaler()
+y = sc_Y.fit_transform(y)
+from sklearn.svm import SVR
+# you should use a suitable kernel
+model = SVR(kernel='rbf')
+model.fit(x,y)
+
+
+y_pred = model.predict([[6.5]])
+
+plt.scatter(x,y,color="blue")
+plt.plot(x,model.predict(x),color="yellow")
+plt.show()
