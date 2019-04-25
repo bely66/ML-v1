8e7260dca95f972ee2fb0956a3cb6f837fd69f01
mohamed nabil <medo661998@gmail.com>
2019-04-25 23:12:25 +0200

    support vector machine

---
+++ Svm/Svm.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 25 22:55:26 2019
+
+@author: USER
+"""
+
+import numpy as np 
+import pandas as pd 
+import matplotlib.pyplot as plt 
+
+data = pd.read_csv("data.csv")
